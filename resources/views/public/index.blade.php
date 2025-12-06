643921eb68e593af1c01d08cb07e4ead4dbe22e1
Natthapoom-036 <67319010036@sbtc.ac.th>
2025-12-06 16:05:05 +0700

    Update

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -25,7 +25,9 @@
                                 @auth
                                     <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 hover:text-indigo-600">Dashboard</a>
                                 @else
-                                    <a href="{{ route('login') }}" class="text-sm font-bold text-gray-700 hover:text-indigo-600">Login</a>
+                                    <a href="{{ route('login') }}" class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:ring ring-indigo-300 disabled:opacity-25 transition ease-in-out duration-150">
+                                        Admin Login
+                                    </a>
                                 @endauth
                             </div>
                         @endif
